d4bf2e32a7bc235cf82700893ea3fe1d697df2f7
Orli Feuchtwang <orli.feuchtwang@gmail.com>
2019-02-09 15:32:11 -0800

    First commit

---
+++ public/main.js
@@ -0,0 +1,43 @@
+function signInOut() {
+	console.log(isUserSignedIn());
+	if(isUserSignedIn()) {
+		signOut();
+	} else {
+		signIn();
+	}
+	console.log(isUserSignedIn());
+}
+
+// Signs-in Friendly Chat.
+function signIn() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+}
+
+// Signs-out of Friendly Chat.
+function signOut() {
+  // Sign out of Firebase.
+  firebase.auth().signOut();
+}
+
+// Initiate firebase auth.
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+// Returns the signed-in user's profile Pic URL.
+function getProfilePicUrl() {
+  return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
+}
+
+// Returns the signed-in user's display name.
+function getUserName() {
+  return firebase.auth().currentUser.displayName;
+}
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+  return !!firebase.auth().currentUser;
+}
